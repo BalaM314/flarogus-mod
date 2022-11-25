a2eeb9b76387adf14eae5b490d605bb64d0b59c1
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2022-11-25 11:52:01 +0530

    Update js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,13 @@
 Events.on(EventType.ContentInitEvent, cons(() => {
 	const flarogus = Vars.content.unit("flarogus-mod-flarogus");
+	
 	//Add eclipse -> flarogus upgrade
 	Blocks.exponentialReconstructor.addUpgrade(UnitTypes.eclipse, flarogus);
-	flarogus.weapons.get(0).shoot = new ShootSpread();
-	flarogus.weapons.get(0).shoot.shots = 30;
-	flarogus.weapons.get(0).shoot.spread = 3;
-	flarogus.weapons.get(0).shoot.shotDelay = 0.5;
+	
+	//Make the flarogus's shoot pattern
+	const pattern = new ShootSpread();
+	pattern.shots = 30;
+	pattern.spread = 3;
+	pattern.shotDelay = 0.5;
+	flarogus.weapons.get(0).shoot = pattern;
 }));
